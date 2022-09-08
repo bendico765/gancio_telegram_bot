ac4b8b110c186c1a3319158ab0c34c5e4a1456eb
bendico765 <bendico765@gmail.com>
2022-09-08 17:24:53 +0200

    Added default cache duration if not specified

--- gancio_telegram_bot/config.py
+++ gancio_telegram_bot/config.py
@@ -9,10 +9,16 @@ instance_url = os.getenv("INSTANCE_URL")
 show_event_url = os.getenv("SHOW_EVENT_URL") == "True"
 elements_in_menu = int(os.getenv("MENU_ELEMENTS")) if os.getenv("MENU_ELEMENTS") is not None else 3
 
-if os.getenv("CACHE_NAME") is not None and os.getenv("HTTP_CACHE_EXPIRATION_SECONDS") is not None:
-	cache = SQLiteBackend(
-		cache_name=os.getenv("CACHE_NAME"),
-		expire_after=int(os.getenv("HTTP_CACHE_EXPIRATION_SECONDS"))
-	)
+if os.getenv("CACHE_NAME") is not None:
+	if os.getenv("HTTP_CACHE_EXPIRATION_SECONDS") is not None:
+		cache = SQLiteBackend(
+			cache_name=os.getenv("CACHE_NAME"),
+			expire_after=int(os.getenv("HTTP_CACHE_EXPIRATION_SECONDS"))
+		)
+	else:
+		cache = SQLiteBackend(
+			cache_name=os.getenv("CACHE_NAME"),
+			expire_after=600
+		)
 else:
 	cache = None
